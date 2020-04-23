a37b3c1b0db4c0f4b93bd9aecf5011f628ff9c90
NOBUOSAN <gariben.elsha.a27scrv@gmail.com>
2020-04-24 01:13:15 +0900

    initial setting

--- js/main.js
+++ js/main.js
@@ -1,56 +1,9 @@
 (function(){
   'use strict';
 
-  var vm  = new Vue({
+  var app  = new Vue({
     el: '#app',
-    data: {
-      newItem: '',
-      todos:[]
-    },
-    watch:{
-      // todos: function(){
-      //   localStorage.setItem('todos', JSON.stringify(this.todos));
-      //   alert('---');
-      // }
-      todos: {
-        handler: function () {
-          localStorage.setItem('todos', JSON.stringify(this.todos));
-          alert('date saved');
-        },
-        deep: true
-      }
-    },
-    mounted: function(){
-      this.todos= JSON.parse(localStorage.getItem('todos')) || [];
-    },
-    methods:{
-      addItem: function(){
-        var item = {
-          title:this.newItem,
-          isDone:false
-        }
-        this.todos.push(item)
-        this.newItem = '';
-      },
-      deleteItem: function(index){
-        if(confirm('are you sure?')){
-          this.todos.splice(index,1);
-        }
-      },
-      purge: function(){
-        if(!confirm('delete finish?')){
-          return;
-        }
-        this.todos = this.remaining;
-
-      }
-    },
-    computed: {
-      remaining: function(){
-        return this.todos.filter(function(todo){
-            return !todo.isDone;
-        });
-      }
-    }
   });
+
+
 })();
